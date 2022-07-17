0995a7e69245201fbee98aa58fa4d61d5788deb0
copokbl <zaneharrison2006@icloud.com>
2022-07-17 14:22:06 +1000

    Implemented /api/v1/account endpoint for apps

--- Data/Schemas/AuthorizationHeader.cs
+++ Data/Schemas/AuthorizationHeader.cs
@@ -7,36 +7,44 @@ public class AuthorizationHeader {
     [FromHeader]
     public string Authorization { get; set; }
     
-    public bool Check(string appId, out string[]? scopes, out User? user) {
+    public bool Check(string appId, out string[]? scopes, out User? user, out string? msg) {
         scopes = null;
         user = null;
+        msg = null;
         
         if (string.IsNullOrEmpty(Authorization)) {
+            msg = "Authorization header is missing";
             return false;
         }
         
         string[] parts = Authorization.Split(' ');
         if (parts.Length != 2) {
+            msg = "Header is not in the correct format";
             return false;
         }
         
         if (parts[0] != "Bearer") {
+            msg = "Header is not Bearer";
             return false;
         }
         
         // Find app
         Program.StorageService!.GetOAuthApp(appId, out OAuthApp? app);
         if (app == null) {
+            msg = "App null";
             return false;
         }
         
         TokenHandler tokenHandler = new TokenHandler(Program.Config!);
         if (!tokenHandler.ValidateCurrentToken(parts[1], out Dictionary<string, string>? claims, app)) {
+            msg = "Token validation failed";
             return false;
         }
         
         scopes = ScopeHandler.StringToListOfScopeIds(claims!["scope"]);
-        
+        Program.StorageService.GetUser(claims["id"], out user);
+
+        msg = "Check success";
         return true;
     }
     
--- api/v1/AccountController.cs
+++ api/v1/AccountController.cs
@@ -1,14 +1,40 @@
+using GeneralPurposeLib;
 using Microsoft.AspNetCore.Mvc;
+using SerbleWebsite.Data;
+using SerbleWebsite.Data.Schemas;
 
 namespace SerbleWebsite.api.v1; 
 
 [ApiController]
-[Route("api/v1/account")]
+[Route("api/v1/account/{userid}")]
 public class AccountController : Controller {
     
     [HttpGet]
-    public IActionResult Index([FromQuery] string appId) {
-        return Ok();
+    public ActionResult<SanitisedUser> Index(string userid, [FromQuery] string appId, [FromHeader] AuthorizationHeader? authorizationHeader) {
+        // Check if the app has access to the user's account
+        if (authorizationHeader == null) {
+            Logger.Debug("No authorization header provided");
+            return Unauthorized();
+        }
+
+        if (!authorizationHeader.Check(appId, out string[]? scopes, out User? user, out string? msg)) {
+            Logger.Debug("Check failed: " + msg);
+            return Unauthorized();
+        }
+
+        if (scopes == null || user == null) {
+            Logger.Debug("NULL");
+            return Unauthorized();
+        }
+
+        return new SanitisedUser(user, ScopeHandler.ListOfScopeIdsToString(scopes));
     }
     
+}
+
+public class Adam {
+    public Adam() {
+        Logger.Error("OH NO U UNLEASHED ADAM");
+        throw new Exception("Adam");
+    }
 }
