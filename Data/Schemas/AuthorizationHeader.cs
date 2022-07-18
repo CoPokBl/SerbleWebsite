4395d2089b521f64fae1dbdbb1865a8f97674757
copokbl <zaneharrison2006@icloud.com>
2022-07-18 17:38:17 +1000

    Made auth header custom

--- Data/Schemas/AuthorizationHeader.cs
+++ Data/Schemas/AuthorizationHeader.cs
@@ -5,37 +5,24 @@ namespace SerbleWebsite.Data.Schemas;
 public class AuthorizationHeader {
     
     [FromHeader]
-    public string Authorization { get; set; }
-    
-    [FromHeader]
-    public string X_App_Secret { get; set; } 
-    
+    public string SerbleAuth { get; set; }
+
     public bool Check(string appId, out string[]? scopes, out User? user, out string? msg) {
         scopes = null;
         user = null;
         msg = null;
         
-        if (string.IsNullOrEmpty(Authorization)) {
+        if (string.IsNullOrEmpty(SerbleAuth)) {
             msg = "Authorization header is missing";
             return false;
         }
 
-        if (string.IsNullOrEmpty(X_App_Secret)) {
-            msg = "X_App_Secret header is missing";
-            return false;
-        }
-        
-        string[] parts = Authorization.Split(' ');
+        string[] parts = SerbleAuth.Split(' ');
         if (parts.Length != 2) {
             msg = "Header is not in the correct format";
             return false;
         }
-        
-        if (parts[0] != "Bearer") {
-            msg = "Header is not Bearer";
-            return false;
-        }
-        
+
         // Find app
         Program.StorageService!.GetOAuthApp(appId, out OAuthApp? app);
         if (app == null) {
@@ -49,7 +36,7 @@ public class AuthorizationHeader {
             return false;
         }
 
-        if (app.ClientSecret != X_App_Secret) {
+        if (app.ClientSecret != parts[0]) {
             msg = "X_App_Secret is not correct";
             return false;
         }
