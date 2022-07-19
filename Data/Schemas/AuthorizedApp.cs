3e121588ef3ba961f7f81aa911efbc19e7a7bce6
copokbl <zaneharrison2006@icloud.com>
2022-07-19 17:35:21 +1000

    Overrode authed apps equality operators

--- Data/Schemas/AuthorizedApp.cs
+++ Data/Schemas/AuthorizedApp.cs
@@ -13,4 +13,24 @@ public class AuthorizedApp {
         AppId = "";
         Scopes = "";
     }
+
+    public static bool operator ==(AuthorizedApp a1, AuthorizedApp a2) {
+        return a1.AppId == a2.AppId && a1.Scopes == a2.Scopes;
+    }
+    
+    public static bool operator !=(AuthorizedApp a1, AuthorizedApp a2) {
+        return !(a1 == a2);
+    }
+    
+    public override bool Equals(object? obj) {
+        if (obj is AuthorizedApp app) {
+            return this == app;
+        }
+        return false;
+    }
+
+    protected bool Equals(AuthorizedApp other) {
+        return this == other;
+    }
+    
 }
