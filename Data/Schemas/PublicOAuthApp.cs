1c199f8c955c2e1bcee71fe694929887ecdcddb3
copokbl <copokbl@serble.net>
2022-12-27 17:10:56 +1100

    Fixed warnings and all broken links

--- Data/Schemas/PublicOAuthApp.cs
+++ Data/Schemas/PublicOAuthApp.cs
@@ -2,7 +2,7 @@ namespace SerbleWebsite.Data.Schemas;
 
 // DO NOT REMOVE SETTERS OR MAKE THEM PRIVATE, IT BREAKS THE JSON SERIALIZATION
 public class PublicOAuthApp {
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
 }
